46826707ebf519b6777bb114e85fa355b07ce299
Cybertechnnp <115475827+Cybertechnnp@users.noreply.github.com>
2023-02-03 23:21:08 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "<!DOCTYPE html>";
+echo "<html lang='en'>";
+echo "<head>";
+echo "  <meta charset='UTF-8'>";
+echo "  <meta http-equiv='X-UA-Compatible' content='IE=edge'>";
+echo "  <meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+echo "  <title>power point</title>";
+echo "</head>";
+echo "<body>";
+    echo "<h1>Hello coder its me bimal</h1>";
+echo "  <div style='position: relative; width: 100%; height: 0; padding-top: 56.2500%;";
+echo " padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;";
+echo " border-radius: 8px; will-change: transform;'>";
+echo "    <iframe loading='lazy' style='position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;'";
+echo "      src='https://www.canva.com/design/DAFZV321YGs/view?embed' allowfullscreen='allowfullscreen' allow='fullscreen'>";
+echo "    </iframe>";
+echo "  </div>";
+echo "</h1>";
+echo "</html>";
+?>
